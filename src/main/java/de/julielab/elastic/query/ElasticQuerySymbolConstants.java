99269f3bf84eba58f05da10b35af61750ad208a7
khituras <chew@gmx.net>
2017-01-11 13:06:56 +0100

    Version 1.0.0. Changed symbol names.

--- src/main/java/de/julielab/elastic/query/ElasticQuerySymbolConstants.java
+++ src/main/java/de/julielab/elastic/query/ElasticQuerySymbolConstants.java
@@ -1,8 +1,8 @@
 package de.julielab.elastic.query;
 
 public class ElasticQuerySymbolConstants {
-	public static final String ES_CLUSTER_NAME = "semedico.searchserver.es.clustername";
+	public static final String ES_CLUSTER_NAME = "semedico.elastic.clustername";
 
-	public static final String ES_HOST = "semedico.searchserver.es.url";
-	public static final String ES_PORT = "semedico.searchserver.es.port";
+	public static final String ES_HOST = "semedico.elastic.url";
+	public static final String ES_PORT = "semedico.elastic.port";
 }
