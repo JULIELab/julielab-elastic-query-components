f2ec5f27b031be29be29ddcdbc6949630af13fee
khituras <chew@gmx.net>
2018-10-19 12:52:06 +0200

    Adding server response classes.
    However I really think they should already be there, I don't exactly
    know why I can add them (again?).

---
+++ src/main/java/de/julielab/elastic/query/services/IElasticServerResponse.java
@@ -0,0 +1,34 @@
+package de.julielab.elastic.query.services;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import de.julielab.elastic.query.components.data.ISearchServerDocument;
+import de.julielab.elastic.query.components.data.QueryError;
+import de.julielab.elastic.query.components.data.aggregation.AggregationRequest;
+import de.julielab.elastic.query.components.data.aggregation.IAggregationResult;
+
+public interface IElasticServerResponse extends ISearchServerResponse {
+
+
+	/**
+	 * Returns the document results of the respective search request. For
+	 * multi-batch requests like the ElasticSearch scroll request, this method
+	 * wraps subsequent batch calls into the stream. Thus, depleting the stream
+	 * will cause all requested documents to be read.
+	 * 
+	 * @return A Stream of search server documents reflecting the documents in the search response.
+	 */
+	Stream<ISearchServerDocument> getDocumentResults();
+
+	IAggregationResult getAggregationResult(AggregationRequest aggCmd);
+
+	long getNumSuggestions();
+
+	List<ISearchServerDocument> getSuggestionResults();
+
+	boolean isSuggestionSearchResponse();
+
+	void setSuggestionSearchResponse(boolean isSuggestionSearchResponse);
+	
+}
